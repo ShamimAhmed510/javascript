8bee5669dbff62e5388d21581797a1b871e597e7
Shamim Ahmed <shamimahmedraju510gmail.com>
2026-07-23 02:33:57 +0600

    filer Exercise

---
+++ Flter.js
@@ -0,0 +1,5 @@
+
+//  // 3. Use filter to keep only names longer than 4 characters from an array of names.
+const name = ["shamim","tal","Abir" , "Sakiib"];
+const names = name.filter((n) => n.length > 4);
+console.log(names);
