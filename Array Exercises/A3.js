62a6d3a8a67f6e934e32e11048597c29767b6037
Shamim Ahmed <shamimahmedraju510gmail.com>
2026-07-24 03:04:29 +0600

    A3

---
+++ Array Exercises/A3.js
@@ -0,0 +1,17 @@
+const days = [
+  "Friday",
+  "Saturday",
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "thrusday",
+];
+
+const a = days.slice(0, 2);
+
+console.log(a);
+
+const b = days.slice(4, 7);
+
+console.log(b);
