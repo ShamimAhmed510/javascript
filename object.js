a9ac763341a056598ae24ceb19b43dbfd70259d3
Shamim Ahmed <shamimahmedraju510gmail.com>
2026-07-23 14:11:30 +0600

    Objects

---
+++ object.js
@@ -0,0 +1,15 @@
+const students = {
+  name: "Shamim",
+  id: "231 - 115 - 214",
+  cgpa: 3.81,
+};
+
+console.log(students.name);
+console.log(students["cgpa"]);
+
+students.dept = "CSE";
+console.log(students["dept"]);
+
+students.section = "F";
+
+console.log(students);
