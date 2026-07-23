e53abd06f5769be27cf04e7cde85a0c767feba91
Shamim Ahmed <shamimahmedraju510gmail.com>
2026-07-23 14:31:28 +0600

    Destructing

---
+++ Destructuring.js
@@ -0,0 +1,12 @@
+const user = {
+  name: "Nabila",
+  city: "Sylhet",
+  role: "Admin"
+}; 
+
+const {name , city} = user 
+console.log(name , city)
+
+const arry = [ 10, 20, 30, 40];
+const [first , , second] = arry
+console.log(first, second)
