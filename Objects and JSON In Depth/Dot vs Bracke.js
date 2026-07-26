4dd0d00dd23a1bb3d3ea6541b381b60fe4edab71
Shamim Ahmed <shamimahmedraju510gmail.com>
2026-07-27 02:37:47 +0600

    Upload Dot and Bracket

---
+++ Objects and JSON In Depth/Dot vs Bracke.js
@@ -0,0 +1,18 @@
+const student = {
+    name: "Shamim",
+    cg: 4.00,
+    "Home Town": "Sylhet"
+};
+console.log(student)
+
+const field = "cg"
+
+console.log(student[field]) //4.00
+console.log(student.field) // undefine 
+console.log(student.cg) //4.00
+
+console.log(student["Home Town"])
+
+
+
+
