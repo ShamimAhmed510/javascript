ddfda43afc6c47655a863aa80dce3af044b66f06
Shamim Ahmed <shamimahmedraju510gmail.com>
2026-07-27 02:30:46 +0600

    Object

---
+++ Objects and JSON In Depth/object syntex.js
@@ -0,0 +1,15 @@
+const student = {
+ // key : values ;
+ name: "Abir",
+ id: "231-115-214",
+ cg: 3.81 ,
+ isRegular : true,
+ dept: "CSE"
+};
+
+console.log(student)
+
+console.log(student.name)
+console.log(student.cg)
+
+
