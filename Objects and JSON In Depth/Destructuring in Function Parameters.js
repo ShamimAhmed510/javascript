b822eee6ee9db2d935edf7273654dfdb5f570298
Shamim Ahmed <shamimahmedraju510gmail.com>
2026-08-08 03:18:31 +0600

    Up

--- Objects and JSON In Depth/Destructuring in Function Parameters.js
+++ Objects and JSON In Depth/Destructuring in Function Parameters.js
@@ -1,6 +1,13 @@
-function makecard({name, id, dept="CSE"})
-{
-    return `Name: ${name}, ID: ${id}, Dept: ${dept}`;
+// function makecard({name, id, dept="CSE"})
+// {
+//     return `Name: ${name}, ID: ${id}, Dept: ${dept}`;
+// }
+
+// console.log(makecard({name:"Shamim", id:214}));
+
+function makecard({name, city = "Dhaka", age}){
+    return `Name: ${name}, City: ${city}, Age: ${age}`;
 }
 
-console.log(makecard({name:"Shamim", id:214}));
+console.log(makecard({name:"Shamim"}));
+console.log(makecard({age: 550}));
