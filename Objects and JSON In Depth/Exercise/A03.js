455bad5dd95897836d65def45d01ce4c13f6b62b
Shamim Ahmed <shamimahmedraju510gmail.com>
2026-08-28 03:00:33 +0600

    Upload

---
+++ Objects and JSON In Depth/Exercise/A03.js
@@ -0,0 +1,16 @@
+const student = {
+  dept: "CSE",
+  cgpa: 3.81,
+};
+
+student.name = "shamim"
+
+console.log(student)
+
+student.name = "Tarek" ;
+
+console.log(student)
+
+delete student.name 
+
+console.log(student)
