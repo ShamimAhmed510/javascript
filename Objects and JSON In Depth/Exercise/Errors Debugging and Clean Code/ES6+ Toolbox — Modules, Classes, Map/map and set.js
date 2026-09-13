11c98e2c5b829192e73e428295f8a1f9ff90f13a
Shamim Ahmed <shamimahmedraju510gmail.com>
2026-09-13 22:38:40 +0600

    Set And Map

--- Objects and JSON In Depth/Exercise/Errors Debugging and Clean Code/ES6+ Toolbox — Modules, Classes, Map/map and set.js
+++ Objects and JSON In Depth/Exercise/Errors Debugging and Clean Code/ES6+ Toolbox — Modules, Classes, Map/map and set.js
@@ -1,4 +1,14 @@
 const set = new Set([1,1,2,2,2,3,4,5,]);
 
-console.log(set.size); // Output: 5
-console.log(...set); // Output: 1 2 3 4 5
+console.log(set.size);     // Output: 5
+console.log(...set);       // Output: 1 2 3 4 5
+
+const mp = new Map();
+
+mp.set("pen", 120 );
+mp.set("pencil", 20 );
+mp.set("bag", 0 );
+
+console.log(mp.get("pen"));     // Output: 120
+
+console.log(mp.has("bag")); // Output: true  bag key exist kore 
