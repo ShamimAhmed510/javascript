5ad0b8562cec3451bfd26264dd7ac4643fdf74ad
Shamim Ahmed <shamimahmedraju510gmail.com>
2026-09-13 22:09:35 +0600

    Splitting Code into Files

---
+++ Objects and JSON In Depth/Exercise/Errors Debugging and Clean Code/ES6+ Toolbox — Modules, Classes, Map/app.js
@@ -0,0 +1,4 @@
+import greet, {add, pi} from './math.js'
+
+console.log(add(2,3), pi); 
+greet();
---
+++ Objects and JSON In Depth/Exercise/Errors Debugging and Clean Code/ES6+ Toolbox — Modules, Classes, Map/math.js
@@ -0,0 +1,10 @@
+
+// 
+
+export const add = (x, y) => x+y ;
+export const pi = 3.14159;
+
+export default function greet(){
+    console.log("Hello, welcome to the module!");
+}
+
