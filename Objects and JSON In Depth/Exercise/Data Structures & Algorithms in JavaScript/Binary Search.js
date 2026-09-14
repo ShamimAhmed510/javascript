0b4d0f0c0d13ecae6c803808aa44770b405eb139
Shamim Ahmed <shamimahmedraju510gmail.com>
2026-09-15 03:58:23 +0600

    Binary SEarch

---
+++ Objects and JSON In Depth/Exercise/Data Structures & Algorithms in JavaScript/Binary Search.js
@@ -0,0 +1,22 @@
+function BinarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+const sorted = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91];
+console.log(BinarySearch(sorted, 23)); // 5
+console.log(BinarySearch(sorted, 4)); // -1
